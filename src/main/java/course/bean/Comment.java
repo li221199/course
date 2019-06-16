bebb50b5744ee327b1a7ad16aa315fd3adce6472
li221199 <86066930@qq.com>
2019-06-16 21:34:35 +0800

    0616

--- src/main/java/course/bean/Comment.java
+++ src/main/java/course/bean/Comment.java
@@ -5,21 +5,14 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Range;
 
 public class Comment
-{
-	@NotNull
-	private String type;	//username
+{	
+	
+	private String CommentId;
 	@Range(min=0,max=100,message="超出0-100分数限制")
-	private String grade;	//打分数
 	private String image;	//图片
 	private String description;	//文字描述
 	@Range(min=1,max=5,message="星级只能选1-5星整数")
-	private String star;	//星级评价
-	public String getGrade() {
-		return grade;
-	}
-	public void setGrade(String grade) {
-		this.grade = grade;
-	}
+	private String star;	//星级评价  
 	public String getImage() {
 		return image;
 	}
@@ -38,12 +31,10 @@ public class Comment
 	public void setStar(String star) {
 		this.star = star;
 	}
-
-	public String getType() {
-		return type;
+	public String getCommentId() {
+		return CommentId;
 	}
-
-	public void setType(String type) {
-		this.type = type;
+	public void setCommentId(String commentId) {
+		CommentId = commentId;
 	}
 }
